6cb88efa2b1616fe50af0d5cd80e13da58971a1c
Patrick Longo <ck.longo@bol.com.br>
2019-10-21 11:24:10 -0300

    Done

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -14,8 +14,8 @@ export default async (req, res, next) => {
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-
-    console.log(decoded);
+    // console.log(decoded); Retorna no console informações do usuario logado/autenticado.
+    req.userId = decoded.id
     return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
